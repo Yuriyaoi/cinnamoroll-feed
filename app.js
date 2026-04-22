d907d3b2d89c8ce2e14c1cf08ddb13cecabcd688
Paveenarat Niphanutiyan <yui1751@hotmail.com>
2026-04-22 23:50:37 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+(function () {
+    let score = 0; // ตัวแปรเก็บคะแนน เริ่มที่ 0
+
+    // ส่วน Logic (Process)
+    window.feedCinnamoroll = function () {
+        score = score + 1; // บวกเพิ่มทีละหนึ่ง
+        document.getElementById("score").innerText = score; // โชว์คะแนนใหม่
+
+        let img = document.getElementById("cinnamoroll-img");
+        let status = document.getElementById("status-text");
+
+        // 🧠 นี่คือ Logic Range (0-50, 51-80, 81+)
+        if (score <= 30) {
+            // State: ผอมโซ
+            img.src = "cinnamoroll_thin.png";
+            img.style.width = "250px";
+            status.innerText = "กำลังหิวโหย...";
+        } else if (score <= 80) {
+            // State: เริ่มมีกิน
+            img.src = "cinnamoroll_normal.png";
+            img.style.width = "300px"; // ขยายขนาดนิดหน่อย
+            status.innerText = "เริ่มมีแก้มแล้ว!";
+        } else {
+            // State: SUPER FAT (เวอร์ชันพุงป่องอิ่มจนเดินไม่ไหว!)
+            img.src = "cinnamoroll_super_fat.png";
+            img.style.width = "400px"; // ขยายร่างใหญ่สุดๆ
+            status.innerText = "อิ่มจนพุงกาง เดินไม่ไหวแล้ว!! (Super Fat)";
+        }
+    };
+})();
